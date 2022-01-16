512f1f53d6273852ffee299ce82c27ebc78331c2
rladuswl <khkkgon18@naver.com>
2022-01-16 21:08:08 +0900

    문제풀이

---
+++ AA/k번째수.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open("input.txt", "rt")
+
+## k번째 수
+T = int(input())  # 테스트 케이스가 몇 번인지
+
+for t in range(1, T+1):
+    n, s, e, k = map(int, input().split())
+
+    a = list(map(int, input().split()))
+    b = []
+    for j in range(s, e+1):
+        b.append(a[j-1])
+    b = sorted(b)
+
+    for i in range(1, len(b)+1):
+        if i == k:
+            print("#{} {}".format(t, b[i-1]))
---
+++ AA/k번째약수.py
@@ -0,0 +1,21 @@
+import sys
+# sys.stdin = open("input.txt", "rt")
+
+## K번째 약수
+N, K = map(int, input().split())
+
+n = 0  # 약수의 개수
+
+for i in range(1, N+1):
+    if N%i == 0:  # N의 약수라면
+        n+=1
+    if n>=K:
+        print(i)
+        break
+else:  # for문 안에서 break를 당하지 않고 나오면(for문 끝까지 돌고 나오면) for-else 실행
+    print(-1)
+
+'''
+if n<K:
+    print(-1)
+'''
