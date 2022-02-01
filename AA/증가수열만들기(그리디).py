cb5531baca9cae511dfd1a4ef3db8f5909a043e7
rladuswl <khkkgon18@naver.com>
2022-02-01 17:22:03 +0900

    문제풀이

---
+++ AA/증가수열만들기(그리디).py
@@ -0,0 +1,34 @@
+import sys
+#sys.stdin = open("input.txt", "rt")
+
+## 증가수열 만들기 (그리디)
+n = int(input())
+a = list(map(int, input().split()))
+
+lt = 0
+rt = n-1
+
+last = 0
+tmp = []
+s = ''
+
+while lt <= rt:
+    if a[lt] > last:
+        tmp.append((a[lt], 'L'))
+    if a[rt] > last:
+        tmp.append((a[rt], 'R'))
+    tmp.sort()
+
+    if len(tmp) == 0:
+        break
+    else:
+        s = s + tmp[0][1]
+        last = tmp[0][0]
+        if tmp[0][1] == 'L':
+            lt += 1
+        else:
+            rt -= 1
+    tmp.clear()
+
+print(len(s))
+print(s)
