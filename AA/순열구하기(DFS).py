257e8c05a1b5abd10413ba4e8954f8846b6fa339
rladuswl <khkkgon18@naver.com>
2022-03-02 00:30:26 +0900

    문제풀이

--- AA/순열구하기(DFS).py
+++ AA/순열구하기(DFS).py
@@ -1,28 +1,62 @@
 import sys
-#sys.stdin = open("input.txt", "rt")
+sys.stdin = open("input.txt", "rt")
 input = sys.stdin.readline
 
-## 순열 구하기
+## 수열 추측하기(순열, 파스칼 응용)
 
-def DFS(L):  # L은 뽑는 개수
-    global cnt
-    if L == m:
-        for j in range(len(res)):
-            print(res[j], end=' ')
-        cnt += 1
-        print()
+'''내 답안
+def DFS(L):
+    if L == n:
+        sum = 0
+        for j in range(len(p)):
+            sum += p[j]*b[j]
+        if sum == f:
+            for k in range(len(p)):
+                print(p[k], end=' ')
+            print()
+            sys.exit(0)
     else:
         for i in range(1, n+1):
-            if ch[i] != 1:  # 중복 체크
-                res[L] = i
+            if ch[i] != 1:
+                p[L] = i
                 ch[i] = 1
                 DFS(L+1)
-                ch[i] = 0  # 되돌리기
+                ch[i] = 0
 
 
-n, m = map(int, input().split())
-res = [0]*m
-cnt = 0
+n, f = map(int, input().split())
+b = [1]*n
+p = [0]*n
 ch = [0]*(n+1)
+
+for i in range(1, n):
+    b[i] = (b[i-1]*(n-i))//i
+
 DFS(0)
-print(cnt)
+'''
+
+'''모범답안'''
+def DFS(L, sum):
+    if L == n and sum == f:
+        for x in p:
+            print(x, end=' ')
+        sys.exit(0)
+    else:
+        for i in range(1, n+1):
+            if ch[i] == 0:
+                ch[i] = 1
+                p[L] = i
+                DFS(L+1, sum+(p[L]*b[L]))
+                ch[i] = 0
+
+
+
+n, f = map(int, input().split())
+b = [1]*n
+p = [0]*n
+ch = [0]*(n+1)
+
+for i in range(1, n):
+    b[i] = (b[i-1]*(n-i))//i
+
+DFS(0, 0)
