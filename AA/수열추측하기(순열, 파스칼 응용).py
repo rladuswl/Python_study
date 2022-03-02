b1c294d2edc2d4f01bcfe255ce8fbae368bfd34f
rladuswl <khkkgon18@naver.com>
2022-03-03 00:00:33 +0900

    문제풀이

---
+++ AA/수열추측하기(순열, 파스칼 응용).py
@@ -0,0 +1,27 @@
+## 수열 추측하기(순열, 파스칼 응용)
+
+
+def DFS(L, sum):
+    if L == n and sum == f:
+        for x in p:
+            print(x, end=' ')
+        sys.exit(0)
+    else:
+        for i in range(1, n+1):
+            if ch[i] == 0:
+                ch[i] = 1
+                p[L] = i
+                DFS(L+1, sum+(p[L]*b[L]))
+                ch[i] = 0
+
+
+
+n, f = map(int, input().split())
+b = [1]*n
+p = [0]*n
+ch = [0]*(n+1)
+
+for i in range(1, n):
+    b[i] = (b[i-1]*(n-i))//i
+
+DFS(0, 0)
