7ec771c560b2e3f26421f6b46c25f259139ac385
rladuswl <khkkgon18@naver.com>
2022-03-20 00:24:28 +0900

    문제풀이

--- solved/1015(수열정렬)(정렬).py
+++ solved/1015(수열정렬)(정렬).py
@@ -1,27 +1,44 @@
 ## 백준 1015 수열 정렬
 
+'''내풀이
+## 백준 1015 수열 정렬
+
 import sys
 input = sys.stdin.readline
 
+n = int(input())
+A = list(map(int, input().split()))
 
-import math
+A_sort = sorted(A)
+P = []
+for i in range(len(A)):
+    idx = A_sort.index(A[i])
+    P.append(idx)
+    A_sort[idx] = '.'
+
+for x in P:
+    print(x, end=' ')
+'''
+
+import sys
+input = sys.stdin.readline
 
 A_size = int(sys.stdin.readline())
 A = sys.stdin.readline().replace("\n", "").split(' ')
 A = [int(i) for i in A]
 
-# A를 오름차순으로 정렬하여 작은 숫자부터 순서대로 정리된 새로운 list를 할당
 sorted_A = [i for i in A]
 sorted_A.sort()
 
 P = []
-# A의 각 숫자들에 대해 sorted_A에서의 index를 찾아 몇번째로 작은 숫자인지 P 수열에 새롭게 append함.
+
 for i in A:
     P.append(sorted_A.index(i))
-    # 이미 할당한 숫자는 sorted_A에서 -1로 대채해 재탐색되지 않도록 함.
+    # 이미 할당한 숫자는 sorted_A에서 -1로 대체해 재탐색되지 않도록 함.
     sorted_A[sorted_A.index(i)] = -1
 
 results = [i for i in P]
 
 for result in results:
     sys.stdout.write(str(result) + ' ')
+
