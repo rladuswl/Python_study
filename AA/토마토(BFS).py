f27fa82082cda6e7f83f949f7e62e484943431f3
kyj <khkkgon0118@gmail.com>
2022-06-16 00:36:07 +0900

    문제풀이

--- AA/토마토(BFS).py
+++ AA/토마토(BFS).py
@@ -3,11 +3,45 @@ sys.stdin = open("input.txt", "rt")
 input = sys.stdin.readline
 
 # 토마토 (BFS 활용)
-# 1. 모든 토마토가 익어있는 상태인지 확인
-# 2. 이중 for문으로 한 칸씩 접근하면서 1이면 bfs 호출하고 그 주변의 안 익은 토마토(0)를 익은 상태(1)로 바꾸기
-# 3. 만약 익은 토마토가 하나도 없다면 -1 출력
 
-n, m = int(input().split())
-graph = [[]*n for _ in range(m)]
+from collections import deque
 
-print(graph)
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(m)]
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+dis = [[0]*n for _ in range(m)]
+
+Q = deque()
+
+for i in range(m):
+    for j in range(n):
+        if graph[i][j] == 1:
+            Q.append((i, j))
+
+while Q:
+    a, b = Q.popleft()
+    for x in range(4):
+        aa = a + dx[x]
+        bb = b + dy[x]
+        if (0<=aa<m) and (0<=bb<n) and (graph[aa][bb]==0):
+            graph[aa][bb]=1
+            dis[aa][bb]=dis[a][b]+1
+            Q.append((aa, bb))
+
+flag = 1
+for i in range(m):
+    for j in range(n):
+        if graph[i][j] == 0:
+            flag = 0
+result = 0
+if flag == 1:
+    for i in range(m):
+        for j in range(n):
+            if dis[i][j] > result:
+                result = dis[i][j]
+    print(result)
+else:
+    print(-1)
