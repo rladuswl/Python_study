0acb29e89246e51fd417cd7d1fd9c719fb3b037c
kyj <khkkgon0118@gmail.com>
2022-06-28 01:53:00 +0900

    문제풀이

--- solved/5597.py
+++ solved/5597.py
@@ -1,9 +1,13 @@
+# 런타임 에러
+
 lst = []
 for _ in range(28):
     x = int(input())
     lst.append(x)
 
 lst.sort()
-print(lst)
 
-
+for i in range(30):
+    if lst[i] != i+1:
+        lst.insert(i, i+1)
+        print(i+1)
