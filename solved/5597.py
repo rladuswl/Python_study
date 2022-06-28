c85b778bfaa4ab4f172279f1cd0468afd68ea990
kyj <khkkgon0118@gmail.com>
2022-06-29 00:16:16 +0900

    문제풀이

--- solved/5597.py
+++ solved/5597.py
@@ -1,4 +1,5 @@
 # 런타임 에러
+# if 추가해서 break 걸었지만 다시 런타임 에러
 
 lst = []
 for _ in range(28):
@@ -7,7 +8,11 @@ for _ in range(28):
 
 lst.sort()
 
+cnt = 0
 for i in range(30):
     if lst[i] != i+1:
         lst.insert(i, i+1)
         print(i+1)
+        cnt += 1
+        if cnt == 2:
+            break
