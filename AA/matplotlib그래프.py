eb5804aa85d9f308369372a1236723d5667f2039
myeondii <khkkgon0118@gmail.com>
2022-09-28 13:09:08 +0900

    문제풀이

---
+++ AA/matplotlib그래프.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 스타일 ggplot
+# x=0~100까지 100 나누기
+# y=3x+7을 plot그래프 짧은 점선 초록색
+plt.style.use('ggplot')
+x = np.linspace(0, 100, 100)
+y = 3*x + 7
+plt.plot(x, y, linestyle='--', color='green')
