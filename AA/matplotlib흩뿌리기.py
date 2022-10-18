d506873ffe9001828451307d09518c42727daab9
myeondii <khkkgon0118@gmail.com>
2022-10-18 21:00:10 +0900

    문제풀이

--- AA/matplotlib흩뿌리기.py
+++ AA/matplotlib흩뿌리기.py
@@ -2,7 +2,7 @@ import numpy as np
 Score = np.random.randint(100, size=(10, 4)) # 국 영 수 과
 
 # x축은 영어, y축은 국어로 흩뿌리기
-import matplotlib.pyplot as plt
+import matplotlib데이터가공.pyplot as plt
 x = Score[:, 1:2]
 y = Score[:, :1]
 plt.scatter(x, y)
